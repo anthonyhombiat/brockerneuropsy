eca2891e7fc09cbe25633360754a8f107366338c
Anthony Hombiat <anthonyhombiat@gmail.com>
2025-12-05 11:43:03 +0100

    Fix section/carousel width

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    scrollSpy();
     smoothScroll();
 
 });
@@ -8,16 +9,32 @@ function smoothScroll() {
     $("nav #menu ul li a, .button").click(function(event) {
         event.preventDefault();
 
-        $("nav #menu ul li a").removeClass("active");
-        $(this).addClass("active");
-
         let hash = this.hash;
-
         $('html, body').animate({
             scrollTop: hash ? $(this.hash).offset().top : 0
         }, 800, function(){
             if(hash)
                 window.location.hash = hash;
+        });
+    });
+}
+
+function scrollSpy(){
+
+    // $("nav #menu ul li a").removeClass("active");
+    // $(this).addClass("active");
+
+    $(window).scroll(function() {
+        const scroll = $(window).scrollTop();
+        $(".hero").each(function() {
+            const offset = $(this).offset().top;
+            if(scroll >= offset && scroll < offset + $(this).height()){
+                const id = $(this).attr("id");
+                $("nav #menu ul li a").removeClass("active");
+                $("nav #menu ul li a[href='#"+id+"']").addClass("active");
+            } else {
+                $("nav #menu ul li a").first().addClass("active");
+            }
         });
     });
 }
